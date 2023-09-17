e7dd4b5a8283c23885961c98a0c13e0f52386b86
Eceeomar <isiomaenuonye@gmail.com>
2023-09-17 05:59:18 +0100

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "letshopeasy",
+  description: "An Ecommerce store where you can buy anything you desire",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
